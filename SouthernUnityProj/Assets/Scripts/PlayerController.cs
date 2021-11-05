dc0cf166db1ac54d19a4f8ac65eacc366b648ece
Russo-ow <e10benga@gmail.com>
2021-11-05 15:13:08 -0500

    Touch controls and device simulator test

--- SouthernUnityProj/Assets/Scripts/PlayerController.cs
+++ SouthernUnityProj/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour, TurnObject {
     }
 
     void Update() {
+        /* Keyboard Input
         if(Input.GetKeyDown(KeyCode.W) && !TileManager.Instance.isObstacle(TilePos().x, TilePos().y + 1)) {
             targetPos = Tile.TileToWorldSpace(TilePos().x, TilePos().y + 1);
             TurnManager.Instance.NewTurn();
@@ -30,6 +31,32 @@ public class PlayerController : MonoBehaviour, TurnObject {
         if(Input.GetKeyDown(KeyCode.D) && !TileManager.Instance.isObstacle(TilePos().x + 1, TilePos().y)) {
             targetPos = Tile.TileToWorldSpace(TilePos().x + 1, TilePos().y);
             TurnManager.Instance.NewTurn();
+        }
+        */
+        if(Input.touchSupported) {
+            if(Input.touchCount > 0) {
+                Touch t = Input.GetTouch(0);
+                RaycastHit hit;
+                if(t.phase == TouchPhase.Began && Physics.Raycast(Camera.main.ScreenToWorldPoint(t.position), Camera.main.transform.forward, out hit)) {
+                    Tile targetTile = TileManager.Instance.getTile(hit.point);
+                    Debug.Log("Touch:" + targetTile.pos);
+                    if(Mathf.Abs(TilePos().x - targetTile.pos.x) + Mathf.Abs(TilePos().y - targetTile.pos.y) == 1 && !targetTile.isObstacle) {
+                        targetPos = Tile.TileToWorldSpace(targetTile.pos);
+                        TurnManager.Instance.NewTurn();
+                    }
+                }
+            }
+        }
+        else {
+            RaycastHit hit;
+            if(Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward, out hit)) {
+                Tile targetTile = TileManager.Instance.getTile(hit.point);
+                Debug.Log("Touch:" + targetTile.pos);
+                if(Mathf.Abs(TilePos().x - targetTile.pos.x) + Mathf.Abs(TilePos().y - targetTile.pos.y) == 1 && !targetTile.isObstacle) {
+                    targetPos = Tile.TileToWorldSpace(targetTile.pos);
+                    TurnManager.Instance.NewTurn();
+                }
+            }
         }
 
         transform.position = Vector3.Lerp(transform.position, targetPos, .2f);
