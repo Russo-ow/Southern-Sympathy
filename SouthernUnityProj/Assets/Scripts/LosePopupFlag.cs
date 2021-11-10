c10dc1c24f22527cad25e4e40aefb714aab16bb3
Kuybey <mailliwmai@gmail.com>
2021-11-09 19:49:23 -0600

    next scene and reload scene

--- SouthernUnityProj/Assets/Scripts/LosePopupFlag.cs
+++ SouthernUnityProj/Assets/Scripts/LosePopupFlag.cs
@@ -1,3 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LosePopupFlag : MonoBehaviour { }
+public class LosePopupFlag : MonoBehaviour {
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+
+
+
+
+
+
+}
--- SouthernUnityProj/Assets/Scripts/WinPopupFlag.cs
+++ SouthernUnityProj/Assets/Scripts/WinPopupFlag.cs
@@ -1,3 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class WinPopupFlag : MonoBehaviour { }
+public class WinPopupFlag : MonoBehaviour {
+
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+
+
+
+}
